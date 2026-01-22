3419ef6f7085a552d9b42c72e8598e5d76774170
Mohammad Alnajdawi <mohammad.alnajdawi@teamviewer.com>
2026-01-22 16:23:29 +0100

    fix: force ipv4 for database connection

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
@@ -28,7 +29,17 @@ func main() {
 	}
 
 	// Connect to Database
-	dbPool, err := pgxpool.New(context.Background(), dbURL)
+	dbConfig, err := pgxpool.ParseConfig(dbURL)
+	if err != nil {
+		log.Fatalf("Unable to parse database URL: %v\n", err)
+	}
+
+	// Force IPv4 because Railway/Docker + Supabase IPv6 can be flaky
+	dbConfig.ConnConfig.DialFunc = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		return (&net.Dialer{}).DialContext(ctx, "tcp4", addr)
+	}
+
+	dbPool, err := pgxpool.NewWithConfig(context.Background(), dbConfig)
 	if err != nil {
 		log.Fatalf("Unable to create database pool: %v\n", err)
 	}
